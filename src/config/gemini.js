da73bc351d8536706a077e468339d706ec73fcf6
Ashwin K N <ashwinkn2004@gmail.com>
2026-01-03 02:30:03 +0530

    partial done

---
+++ src/config/gemini.js
@@ -0,0 +1,39 @@
+
+/*
+ * Install the Generative AI SDK
+ *
+ * $ npm install @google/generative-ai
+ */
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+} from "@google/generative-ai";
+
+const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+    model: "gemini-2.5-flash",
+});
+
+const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+};
+
+async function runChat(prompt, history) {
+    const chatSession = model.startChat({
+        generationConfig,
+        history: history,
+    });
+
+    const result = await chatSession.sendMessage(prompt);
+    return result.response.text();
+}
+
+export default runChat;
